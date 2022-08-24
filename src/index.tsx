604a2dd1f889da2025bc59a5ae925726adacf404
Sweety <opolskaia.alina@gmail.com>
2022-08-24 17:41:50 +0200

    changing tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { render } from "react-dom";
-// import { Provider } from "react-redux";
-// import store from "./js/store/index";
-// import App from "./js/components/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
